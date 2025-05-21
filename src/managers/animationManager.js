c57c9fa2db074f9acf3556b5535c3a23e739e6a4
HJH <nahjh2@gmail.com>
2025-05-21 18:11:24 +0900

    [#11] add AnimationManager skeleton

--- src/managers/animationManager.js
+++ src/managers/animationManager.js
@@ -1,1 +1,35 @@
-class AnimationManager {}
+class AnimationManager {
+  isPlaying = false;
+  _currently_played_animations = [];
+
+  playAnimation(animation) {
+    animation.tick = 0;
+    _currently_played_animations.push(animation);
+
+    if (!this.isPlaying) {
+      this.isPlaying = true;
+      this.render();
+    }
+  }
+
+  pauseAnimation(animation) {
+    this._currently_played_animations =
+      this._currently_played_animations.filter((ani) => ani !== animation);
+
+    if (this.this._currently_played_animations.empty()) {
+      this.isPlaying = false;
+    }
+  }
+
+  render() {
+    if (this.isPlaying == false) {
+      return;
+    }
+
+    this._currently_played_animations.forEach((animation, idx) => {
+      animation.render();
+    });
+
+    window.requestAnimationFrame(this.render.bind(this));
+  }
+}
