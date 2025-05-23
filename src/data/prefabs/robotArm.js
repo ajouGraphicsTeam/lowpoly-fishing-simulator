59004853fff7fe1833de31f98dd77322960b7aa2
cuffyluv <joosw1117@gmail.com>
2025-05-23 13:13:10 +0900

    Feature/add custom colors and coloring per object

--- src/data/prefabs/robotArm.js
+++ src/data/prefabs/robotArm.js
@@ -13,7 +13,10 @@ class RobotArm extends PrefabObject {
       [-0.1, -0.5, -0.025],
     ]);
 
-    const _arm = new HierarchyObject([_box], new Transform());
+    // 가장 아래 팔(테스트 용으로 null 전달했음, 기본 색상인 어두운 노란색으로 설정됨.)
+    const _arm = new HierarchyObject([_box], new Transform(), null);
+
+    // 그 다음 팔(초록색)
     _arm.children = {
       innerArm: new HierarchyObject(
         [_box],
@@ -21,9 +24,12 @@ class RobotArm extends PrefabObject {
           position: vec3(0, 1, 0),
           rotation: vec3(0, 0, 30),
           anchor: vec3(0, 0.5, 0),
-        })
+        }),
+        COLORS.GREEN
       ),
     };
+
+    // 마지막 말단 팔(파란색)
     _arm.children["innerArm"].children = {
       innerArm: new HierarchyObject(
         [_box],
@@ -31,7 +37,8 @@ class RobotArm extends PrefabObject {
           position: vec3(0, 1, 0),
           rotation: vec3(0, 0, 30),
           anchor: vec3(0, 0.5, 0),
-        })
+        }),
+        COLORS.BLUE
       ),
     };
 
